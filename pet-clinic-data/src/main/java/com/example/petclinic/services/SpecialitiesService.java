c5d602e2cef9e33f56659f9fa2f31a498375f980
labtec96 <cezaryhan1@gmail.com>
2020-02-20 19:41:27 +0100

    Added Speciality Map Service Impl closes #31

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,10 @@
+package com.example.petclinic.services;
+
+import com.example.petclinic.model.Speciality;
+
+/**
+ * Created by ch on 2020-02-20
+ */
+public interface SpecialitiesService extends CrudService <Speciality,Long> {
+
+}
