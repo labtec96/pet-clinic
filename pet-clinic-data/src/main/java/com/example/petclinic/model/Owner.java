0c11cf49e42406b968308aba555acfddf7802a74
labtec96 <cezaryhan1@gmail.com>
2020-02-20 11:18:03 +0100

    added visit entity and upload owner with relationship close #26

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package com.example.petclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by ch on 2020-02-11
  */
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
