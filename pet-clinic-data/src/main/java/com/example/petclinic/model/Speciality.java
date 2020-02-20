e019de3bf6015c694d144b752def2d853715e91d
labtec96 <cezaryhan1@gmail.com>
2020-02-20 11:22:02 +0100

    added Speciality entity, relationship with vet close #27

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.example.petclinic.model;
+
+/**
+ * Created by ch on 2020-02-20
+ */
+public class Speciality extends  BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package com.example.petclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by ch on 2020-02-11
  */
 public class Vet extends  Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
