d9b717ad108853088ef5b00f1e4a443f4af15846
labtec96 <cezaryhan1@gmail.com>
2020-02-27 18:47:32 +0100

    implemented map for visit service  closes #46

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package com.example.petclinic.services;
+
+import com.example.petclinic.model.Visit;
+
+/**
+ * Created by ch on 2020-02-27
+ */
+public interface VisitService extends CrudService<Visit,Long> {
+}
