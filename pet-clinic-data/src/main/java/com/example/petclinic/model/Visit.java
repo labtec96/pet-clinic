43df8d727a26e3e6150b9ddb59135965dc971c7c
labtec96 <cezaryhan1@gmail.com>
2020-02-27 13:34:38 +0100

    Update Visit with JPA annotations closes #37

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package com.example.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by ch on 2020-02-20
  */
+@Entity
+@Table(name = "visit")
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
