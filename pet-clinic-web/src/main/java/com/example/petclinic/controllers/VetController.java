f49bd8181bb15758950b7780542233ff98608c5b
labtec96 <cezaryhan1@gmail.com>
2020-04-02 20:16:23 +0200

    implemented json endpoint closes 66

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.example.petclinic.controllers;
 
+import com.example.petclinic.model.Vet;
 import com.example.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Created by ch on 2020-02-12
@@ -23,5 +28,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping({"/api/vets"})
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
